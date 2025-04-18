3cd8b46ac13cc16a5928181cd279e53c18d2be9c
Ivan19156 <ilkiv2007ivan@gmail.com>
2025-04-18 16:24:34 +0300

    DesignTimeDbContextFactory.cs file was added.

---
+++ FreelancePlatform/DesignTimeDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FreelancePlatform
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<FreelancePlatformDbContext>
+    {
+        public FreelancePlatformDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FreelancePlatformDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FreelancePlatformDB;Integrated Security=True;");
+
+            return new FreelancePlatformDbContext(optionsBuilder.Options);
+        }
+    }
+}
